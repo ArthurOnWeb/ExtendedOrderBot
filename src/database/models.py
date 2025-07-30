871c3438fa5f1a65ad99a5032d937f2191cc3b53
ArthurOnWeb <117440022+ArthurOnWeb@users.noreply.github.com>
2025-07-30 11:40:13 +0200

    Alchemy model for orders

--- src/database/models.py
+++ src/database/models.py
@@ -19,5 +19,19 @@ class Trade(Base):
     fees = Column(Float, default=0)
     created_at = Column(DateTime)
     closed_at = Column(DateTime)
-    
+
     orders = relationship("Order", back_populates="trade")
+
+class Order(Base):
+    __tablename__ = 'orders'
+    id = Column(String, primary_key=True)
+    trade_id = Column(String, ForeignKey('trades.id', ondelete='CASCADE'))
+    type = Column(String, nullable=False)
+    side = Column(String, nullable=False)
+    size = Column(Float, nullable=False)
+    price = Column(Float)
+    status = Column(String, nullable=False)
+    exchange_id = Column(String)
+    timestamp = Column(DateTime)
+
+    trade = relationship("Trade", back_populates="orders")
